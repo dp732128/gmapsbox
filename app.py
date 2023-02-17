07089b951ff9d4f41142c860a49ccd6e92143d1e
horsenoiseadministrator <103950749+horsenoiseadministrator@users.noreply.github.com>
2023-02-17 09:29:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import csv
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-
+@app.route('/', methods=['GET'])
 def index():
    print('Request for index page received')
    return render_template('index.html')

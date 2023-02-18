62b2a6b95db8ad6ebceae127ca0efd0dc506d752
horsenoiseadministrator <103950749+horsenoiseadministrator@users.noreply.github.com>
2023-02-18 01:37:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config["DEBUG"] = True
 @app.route('/', methods=['GET'])
 def index():
    print('Request for index page received')
-   return render_template('index.html')
+   return render_template('basic.html')
 
 
 @app.route('/getcoords', methods=['GET'])
